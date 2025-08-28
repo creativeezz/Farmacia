3555fe12d5bf5bb16a5d330de73fca94ac759de8
Luiz Henrique <lhmendes0911@gmail.com>
2025-08-28 15:33:14 -0300

    Feature: Criação do Hello World e inicio da Api Farmacia

---
+++ luiz/src/main/java/com/farmacia/luiz/model/Produtos.java
@@ -0,0 +1,24 @@
+package com.farmacia.luiz.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+@Table(name = "TBPRODUTOS")
+public class Produtos {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CDPRODUTO")
+    private Integer cdProduto;
+    @Column(name = "NMPRODUTO")
+    private String nmProduto;
+    @Column(name = "DSBULA")
+    private String dsBula;
+    @Column(name = "VLPRODUTO")
+    private Double vlProduto;
+}
---
+++ luiz/src/test/java/com/farmacia/luiz/LuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.luiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
